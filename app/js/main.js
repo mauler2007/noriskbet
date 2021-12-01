2114edb7860c8e0f297f516bc203155328392ef8
Your Namemauler2007 <mauler2007@gmail.com>
2021-12-01 20:48:05 +0200

    add  promo popup instruction

--- app/js/main.js
+++ app/js/main.js
@@ -1,56 +1,51 @@
 document.addEventListener("DOMContentLoaded", function () {
   
- 
- 
-  
   // =====start popup
-  // const modalBtn = document.querySelectorAll('[data-modal]');
+  const modalBtn = document.querySelectorAll('[data-modal]');
 
-  // const modal = document.querySelectorAll('.modal');
-  // const body = document.querySelector('body');
+  const modal = document.querySelectorAll('.modal');
+  const body = document.querySelector('body');
 
-  // modalBtn.forEach(item => {
-  //   item.addEventListener('click', event => {
+  modalBtn.forEach(item => {
+    item.addEventListener('click', event => {
 
-  //     let $this = event.currentTarget;
-  //     let modalId = $this.getAttribute('data-modal');
+      let $this = event.currentTarget;
+      let modalId = $this.getAttribute('data-modal');
 
-  //     let modal = document.getElementById(modalId);
-  //     let modalWrapper = modal.querySelector('.popup__wrapper');
+      let modal = document.getElementById(modalId);
+      let modalWrapper = modal.querySelector('.popup__wrapper');
 
-  //     modalWrapper.addEventListener('click', event => {
-  //       event.stopPropagation();
-  //     });
+      modalWrapper.addEventListener('click', event => {
+        event.stopPropagation();
+      });
 
-  //     modal.classList.add('popup-show');
-  //     body.classList.add('no-scroll');
+      modal.classList.add('popup-show');
+      body.classList.add('no-scroll');
 
-  //     setTimeout(function () {
-  //       modalWrapper.style.transform = 'none';
-  //     }, 1);
-  //   });
-  // });
+      setTimeout(function () {
+        modalWrapper.style.transform = 'none';
+      }, 1);
+    });
+  });
 
-  // modal.forEach(item => {
-  //   item.addEventListener('click', event => {
-  //     let currentModal = event.currentTarget
+  modal.forEach(item => {
+    item.addEventListener('click', event => {
+      let currentModal = event.currentTarget
 
-  //     closeModal(currentModal)
-  //   });
-  // });
+      closeModal(currentModal)
+    });
+  });
 
-  // function closeModal(currentModal) {
-  //   let modalWrapper = currentModal.querySelector('.popup__wrapper');
+  function closeModal(currentModal) {
+    let modalWrapper = currentModal.querySelector('.popup__wrapper');
 
-  //   modalWrapper.removeAttribute('style');
-  //   body.classList.remove('no-scroll');
+    modalWrapper.removeAttribute('style');
+    body.classList.remove('no-scroll');
 
 
-  //   setTimeout(function () {
-  //     currentModal.classList.remove('popup-show');
-  //   }, 300);
-  // }
+    setTimeout(function () {
+      currentModal.classList.remove('popup-show');
+    }, 300);
+  }
   // =====start popup
-
-
 });
